b61e543bfae26c63d1e9ff03278da94082b072b8
Schavess <eng.arthurschaves@gmail.com>
2024-05-21 22:11:38 -0300

    MobileStartingWithoutBugs2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { NativeBaseProvider } from 'native-base';
 import { THEME } from './src/theme/';
 
